014f16f2e88e3fade5a858dff2ec34610abbf58c
= <=>
2023-09-08 16:39:56 -0700

    corrections

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,38 +7,38 @@
  */
 int main(void)
 {
-    int tensDigit1, onesDigit1;
+	int tensDigit1, onesDigit1, tensDigit2, onesDigit2, startDigit;
 
-    for (tensDigit1 = 0; tensDigit1 < 10; tensDigit1++)
-    {
-        for (onesDigit1 = 0; onesDigit1 < 10; onesDigit1++)
-        {
-            int tensDigit2, onesDigit2;
+	for (tensDigit1 = 0; tensDigit1 < 10; tensDigit1++)
+	{
+		for (onesDigit1 = 0; onesDigit1 < 10; onesDigit1++)
+		{
+			for (tensDigit2 = tensDigit1; tensDigit2 < 10; tensDigit2++)
+			{
+				startDigit = (tensDigit1 == tensDigit2) ? onesDigit1 : 0;
 
-            for (tensDigit2 = tensDigit1; tensDigit2 < 10; tensDigit2++)
-            {
-                int startDigit = (tensDigit1 == tensDigit2) ? onesDigit1 : 0;
+				for (onesDigit2 = startDigit; onesDigit2 < 10; onesDigit2++)
+				{
+					if (tensDigit1 == tensDigit2 && onesDigit1 == onesDigit2)
+						continue;
+					putchar(tensDigit1 + '0');
+					putchar(onesDigit1 + '0');
+					putchar(' ');
+					putchar(tensDigit2 + '0');
+					putchar(onesDigit2 + '0');
 
-                for (onesDigit2 = startDigit; onesDigit2 < 10; onesDigit2++)
-                {
-                    putchar(tensDigit1 + '0');
-                    putchar(onesDigit1 + '0');
-                    putchar(' ');
-                    putchar(tensDigit2 + '0');
-                    putchar(onesDigit2 + '0');
+					if (!(tensDigit1 == 9 && onesDigit1 == 9 && tensDigit2 == 9 && onesDigit2 == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
 
-                    if (tensDigit1 != 9 || onesDigit1 != 9 || tensDigit2 != 9 || onesDigit2 != 9)
-                    {
-                        putchar(',');
-                        putchar(' ');
-                    }
-                }
-            }
-        }
-    }
+	putchar('\n');
 
-    putchar('\n');
-
-    return 0;
+	return (0);
 }
 
