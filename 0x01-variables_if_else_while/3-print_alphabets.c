50f3a99fa56d93890f44d399115ad643b6dd6d32
= <=>
2023-09-08 15:30:45 -0700

    corrections

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,21 +7,21 @@
  */
 int main(void)
 {
-    char lowercase;
-    char uppercase;
+	char lowercase;
+	char uppercase;
 
-    for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
-    {
-        putchar(lowercase);
-    }
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		putchar(lowercase);
+	}
 
-    for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
-    {
-        putchar(uppercase);
-    }
+	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+	{
+		putchar(uppercase);
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
 
