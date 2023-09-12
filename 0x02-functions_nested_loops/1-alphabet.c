599c682d6200094ef24d4ceddecf93d9cba24e5b
= <=>
2023-09-12 14:45:52 -0700

    corrections

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * print_alphabet - Prints the lowercase alphabet followed by a new line
- */
+ * print_alphabet - hello world
+*/
 void print_alphabet(void)
 {
-char letter = 'a';
-while (letter<='z')	
-	{
-        	_putchar(letter);
-        	letter++;
-    	}
-_putchar('\n');
-}
+	char i;
 
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,17 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef __HEADER__
+#define __HEADER__
 int _putchar(char c);
 void print_alphabet(void);
-
-#endif /* MAIN_H */
-
+void get_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+#endif
